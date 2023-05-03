df839ff92ea35c37e4c565415480180d36da5950
Ahmed_bizo <ahmed.bizo@evimeria.se>
2023-05-03 09:13:10 +0200

    fixed footer to be responsive

--- components/demoFooter.tsx
+++ components/demoFooter.tsx
@@ -4,17 +4,15 @@ import { CatCardProps } from "@/types/global";
 
 const DemoFooter = (props: { herd: CatCardProps[]; UserData: string }) => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContainer}>
-        <select className={styles.dropdown}>
-          <option className={styles.dropdownOption} value={props.UserData}>
-            {props.UserData}
-          </option>
-        </select>
-        <div className={styles.aligment}>
-          <div className={styles.carAligment}>Car Aligment</div>
-          <div className={styles.truckAligment}>Truck Aligment</div>
-        </div>
+    <footer className={styles.footerContainer}>
+      <select className={styles.dropdown}>
+        <option className={styles.dropdownOption} value={props.UserData}>
+          {props.UserData}
+        </option>
+      </select>
+      <div className={styles.aligment}>
+        <div className={styles.carAligment}>Car Aligment</div>
+        <div className={styles.truckAligment}>Truck Aligment</div>
       </div>
     </footer>
   );
